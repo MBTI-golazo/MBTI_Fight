3e29ecdca919a113502cbd31b72ad36783e253af
James <vkdnjtjsej@gmail.com>
2023-03-07 12:33:56 +0900

    E&Ivo구현_0307_승진

---
+++ MBTI_TeamProject/src/main/java/team/spring/springmbti/survey/vo/Surveyone.java
@@ -0,0 +1,16 @@
+package team.spring.springmbti.survey.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Surveyone {
+	
+	private int num;
+	private int qnum;
+	private int score;
+	
+}
