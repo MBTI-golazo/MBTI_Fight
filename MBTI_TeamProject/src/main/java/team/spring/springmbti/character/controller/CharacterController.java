111a1cf30af38e35e0550fa2140649255386ef0f
noseung-in <dmsgusaus33@naver.com>
2023-03-07 18:53:44 +0900

    대결로직틀완성 2023/03/07_senoh

--- MBTI_TeamProject/src/main/java/team/spring/springmbti/character/controller/CharacterController.java
+++ MBTI_TeamProject/src/main/java/team/spring/springmbti/character/controller/CharacterController.java
@@ -7,6 +7,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import team.spring.springmbti.character.service.CharacterService;
+import team.spring.springmbti.character.vo.CharacterInfo;
+import team.spring.springmbti.user.vo.User;
+
 @Controller
 public class CharacterController {
 
@@ -20,6 +24,16 @@ public class CharacterController {
 	@GetMapping(value = "fight")
 	public String fight(Model model) {
 		log.debug("싸우기");
+		CharacterService service = new CharacterService();
+		
+		CharacterInfo c = new CharacterInfo(1, 100, 11, 9, 6, 4, 11, 10, 10, 30, 5);
+		CharacterInfo d = new CharacterInfo(2, 100, 9, 11, 4, 6, 10, 10, 10, 30 ,5);
+		
+		User u1 = new User();
+		u1.setUserName("도전자");
+		User u2 = new User();
+		u2.setUserName("방어자");
+		String result = service.fight(c, d, u1.getUserName(), u2.getUserName());
 		return null;
 	}
 }
