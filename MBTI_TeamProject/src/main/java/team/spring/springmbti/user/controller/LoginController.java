4da1c38db1c451d93f9e30e386588be0a844c816
user <user@DESKTOP-RS9DJG3>
2023-03-08 16:57:42 +0900

    MBTI 패키지 분리, myPage에서 resultPage가는 임시 버튼
    결과페이지에서 카카오톡으로 결과페이지 공유, 마이페이지로 이동 기능_2023/03/08_재진

--- MBTI_TeamProject/src/main/java/team/spring/springmbti/user/controller/LoginController.java
+++ MBTI_TeamProject/src/main/java/team/spring/springmbti/user/controller/LoginController.java
@@ -8,7 +8,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import team.spring.springmbti.user.service.LoginService;
 import team.spring.springmbti.user.service.OAuthService;
@@ -16,6 +18,7 @@ import team.spring.springmbti.user.vo.User;
 
 
 @Controller
+@SessionAttributes(value= {"myUser"})
 public class LoginController {
 	
 	@Autowired
@@ -29,14 +32,19 @@ public class LoginController {
 	Logger log = LogManager.getLogger("case3");
 	
 	
+	@ModelAttribute("myUser")
+	public User createUser() {
+		User user = new User();
+		return user;
+	}
 	
 	@GetMapping("login")
-    public String home(@RequestParam(value = "code", required = false) String code,Model model) throws Exception{
+    public String home(@RequestParam(value = "code", required = false) String code,Model model,@ModelAttribute("myUser") User user) throws Exception{
         log.debug("#########" + code);
         String access_Token = kakaoservice.getKakaoAccessToken(code);
         HashMap<String, Object> userInfo = kakaoservice.getUserInfo(access_Token);
         log.debug("###access_Token#### : " + access_Token);
-        log.debug("###userInfo#### : " + userInfo.get("email"));
+        log.debug("###useremail#### : " + userInfo.get("email"));
         log.debug("###nickname#### : " + userInfo.get("nickname"));
         log.debug("###profile_image#### : " + userInfo.get("profile_image"));
         
@@ -44,7 +52,7 @@ public class LoginController {
         String userEmail = (String) userInfo.get("email");
         String userProfile = (String) userInfo.get("profile_image");
         
-        User user = new User();
+//        User user = new User();
         user.setUserName(userName);
         user.setUserEmail(userEmail);
         user.setUserProfile(userProfile);
@@ -65,7 +73,10 @@ public class LoginController {
 	@GetMapping("test")
 	public String myTest() {
 		
+		
+		
 		return "resultPage";
 	}
+	
 	
 }
